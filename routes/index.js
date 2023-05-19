5e7229dbba077eeba6454635c2a0d205bbc60ca1
Kseniia Ernest <hramtcovakseniia@gmail.com>
2023-05-19 10:12:51 -0400

    Iteration #1 done

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,12 @@
 const router = require("express").Router();
 
+// connect routes/celebrities.routes.js and routes/movies.routes.js to server:
+const celebritiesRoutes = require('./celebrities.routes');
+router.use('/celebrities', celebritiesRoutes);
+
+const moviesRoutes = require('./movies.routes');
+router.use('/movies', moviesRoutes);
+
 /* GET home page */
 router.get("/", (req, res, next) => {
   res.render("index");
