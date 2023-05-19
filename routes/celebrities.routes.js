730fe157b715a238483db70f1f726686e15047f7
Kseniia Ernest <hramtcovakseniia@gmail.com>
2023-05-19 10:48:31 -0400

    Iteration #3 done

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -1,4 +1,29 @@
 const router = require("express").Router();
+const Celebrity = require("../models/Celebrity.model");
+
+
+// Adding New Celebrity -------*****-------*****-------*****-------*****-------*****-------*****
+router.get('/celebrities/create', (req, res) => {
+  res.render('celebrities/new-celebrity')
+});
+
+router.post('/celebrities/create', (req, res) => {
+  Celebrity.create({
+  name: req.body.theName,
+  occupation: req.body.theOccupation,
+  catchPhrase: req.body.theCatchphrase
+  })
+  .then((response) => {
+    res.redirect('/all-celebrities');
+  })
+  .catch((err) => {
+    res.render('celebrities/new-celebrity');
+  })
+})
+
+
+
+
 
 
 
