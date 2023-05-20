dc1754bc55504a1d1fdf9570d8422020ab47171e
Kseniia Ernest <hramtcovakseniia@gmail.com>
2023-05-20 14:59:18 -0400

    Iteration #6 done

--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -1,5 +1,45 @@
 const router = require("express").Router();
+const Celebrity = require("../models/Celebrity.model");
+const Movie = require('../models/Movie.model');
 
+// All movies page -----------------------**********************-------------------------*************************-
+router.get('/all-movies', (req, res) => {
+  Movie.find().populate('cast')
+  .then((allMovies) => {
+    res.render('movies/movies', {movies: allMovies});
+  })
+  .catch((err) => {
+    console.log(err);
+  })
+ 
+})
+
+//// Adding New Movie -------*****-------*****-------*****-------*****-------*****-------*****
+router.get('/create', (req, res) => {
+  Celebrity.find()
+  .then((allCelebrities) => {
+    res.render('movies/new-movie', {celebrities: allCelebrities})
+  })
+  .catch((err) => {
+    console.log(err);
+  })
+
+});
+
+router.post('/create', (req, res) => {
+  Movie.create({
+    title: req.body.theTitle,
+    genre: req.body.theGenre,
+    plot: req.body.thePlot,
+    cast: req.body.theCelebrity
+  })
+  .then((response) => {
+    res.redirect('/movies/all-movies')
+  })
+  .catch((err) => {
+    console.log(err);
+  })
+})
 
 
 
